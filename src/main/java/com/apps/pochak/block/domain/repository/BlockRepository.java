678e308f542ebd64593fe7e4b0cb00996b7f1c39
Lucy Oh <56earls@gmail.com>
2024-05-30 17:08:10 +0900

    #4 Docs: 차단한 유저 조회 API 문서화

--- src/main/java/com/apps/pochak/block/domain/repository/BlockRepository.java
+++ src/main/java/com/apps/pochak/block/domain/repository/BlockRepository.java
@@ -4,10 +4,12 @@ import com.apps.pochak.block.domain.Block;
 import com.apps.pochak.member.domain.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BlockRepository extends JpaRepository<Block, Long> {
 
+    @EntityGraph(attributePaths = {"blockedMember"})
     Page<Block> findBlockByBlocker(
             final Member blocker,
             final Pageable pageable
--- src/test/java/com/apps/pochak/block/controller/BlockControllerTest.java
+++ src/test/java/com/apps/pochak/block/controller/BlockControllerTest.java
@@ -29,8 +29,7 @@ import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.docu
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.payload.JsonFieldType.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -82,6 +81,66 @@ class BlockControllerTest {
                                         fieldWithPath("isSuccess").type(BOOLEAN).description("성공 여부"),
                                         fieldWithPath("code").type(STRING).description("결과 코드"),
                                         fieldWithPath("message").type(STRING).description("결과 메세지")
+                                )
+
+                        )
+                );
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("Get BlockedMember API Document")
+    void getBlockedMemberTest() throws Exception {
+
+        this.mockMvc.perform(
+                        RestDocumentationRequestBuilders
+                                .get("/api/v2/members/{handle}/block", "dxxynni")
+                                .header("Authorization", authorization)
+                                .contentType(APPLICATION_JSON)
+                ).andExpect(status().isOk())
+                .andDo(
+                        document("get-blocked-member",
+                                getDocumentRequest(),
+                                getDocumentResponse(),
+                                requestHeaders(
+                                        headerWithName("Authorization").description("Basic auth credentials")
+                                ),
+                                pathParameters(
+                                        parameterWithName("handle").description("차단할 사용자의 아이디(handle) : " +
+                                                "만약 로그인 정보와 일치하지 않을 경우 UNAUTHORIZED 에러가 발생합니다. (자신만 차단한 사용자 열람 가능)")
+                                ),
+                                queryParameters(
+                                        parameterWithName("page").description("조회할 페이지 [default: 0]").optional()
+                                ),
+                                responseFields(
+                                        fieldWithPath("isSuccess").type(BOOLEAN).description("성공 여부"),
+                                        fieldWithPath("code").type(STRING).description("결과 코드"),
+                                        fieldWithPath("message").type(STRING).description("결과 메세지"),
+                                        fieldWithPath("result").type(OBJECT).description("결과 데이터"),
+                                        fieldWithPath("result.pageInfo").type(OBJECT).description("차단한 사용자 페이징 정보"),
+                                        fieldWithPath("result.pageInfo.lastPage").type(BOOLEAN)
+                                                .description(
+                                                        "차단한 사용자 페이징 정보: 현재 페이지가 마지막 페이지인지의 여부"
+                                                ),
+                                        fieldWithPath("result.pageInfo.totalPages").type(NUMBER)
+                                                .description(
+                                                        "차단한 사용자 페이징 정보: 총 페이지 수"
+                                                ),
+                                        fieldWithPath("result.pageInfo.totalElements").type(NUMBER)
+                                                .description(
+                                                        "차단한 사용자 페이징 정보: 총 차단된 사용자 수"
+                                                ),
+                                        fieldWithPath("result.pageInfo.size").type(NUMBER)
+                                                .description(
+                                                        "차단한 사용자 페이징 정보: 페이징 사이즈 [default: 30]"
+                                                ),
+                                        fieldWithPath("result.blockList").type(ARRAY).description("차단한 사용자 리스트"),
+                                        fieldWithPath("result.blockList[].profileImage").type(STRING)
+                                                .description("차단한 사용자 리스트: 프로필 이미지").optional(),
+                                        fieldWithPath("result.blockList[].handle").type(STRING)
+                                                .description("차단한 사용자 리스트: 아이디 (handle)").optional(),
+                                        fieldWithPath("result.blockList[].name").type(STRING)
+                                                .description("차단한 사용자 리스트: 이름").optional()
                                 )
 
                         )
