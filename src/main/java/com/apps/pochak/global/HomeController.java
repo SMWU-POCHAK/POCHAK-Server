314acfb01112e3082d5ca8ed8f7600487855256a
영서 <dudrhy12@sookmyung.ac.kr>
2024-08-09 09:58:06 +0900

    #33 Feat: health-check 컨트롤러 추가

--- src/main/java/com/apps/pochak/global/HomeController.java
+++ src/main/java/com/apps/pochak/global/HomeController.java
@@ -2,11 +2,19 @@ package com.apps.pochak.global;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HomeController {
     @GetMapping("/index")
     public String home() {
         return "index";
+    }
+
+    @ResponseBody
+    @GetMapping("/health-check")
+    public String healthCheck() {
+        return "OK";
     }
 }
