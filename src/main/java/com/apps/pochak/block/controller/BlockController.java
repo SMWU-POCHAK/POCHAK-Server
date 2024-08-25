6807cf2ff3cdf3174df293c50450d991adb1dd3f
Lucy Oh <56earls@gmail.com>
2024-08-25 23:53:52 +0900

    #7 Refactor: `BlockController` custom auth annotation으로 변경

--- src/main/java/com/apps/pochak/block/controller/BlockController.java
+++ src/main/java/com/apps/pochak/block/controller/BlockController.java
@@ -1,5 +1,8 @@
 package com.apps.pochak.block.controller;
 
+import com.apps.pochak.auth.Auth;
+import com.apps.pochak.auth.MemberOnly;
+import com.apps.pochak.auth.domain.Accessor;
 import com.apps.pochak.block.dto.response.BlockElements;
 import com.apps.pochak.block.service.BlockService;
 import com.apps.pochak.global.api_payload.ApiResponse;
@@ -19,27 +22,41 @@ public class BlockController {
     private final BlockService blockService;
 
     @PostMapping("/block")
+    @MemberOnly
     public ApiResponse<Void> blockMember(
+            @Auth final Accessor accessor,
             @PathVariable("handle") final String handle
     ) {
-        blockService.blockMember(handle);
+        blockService.blockMember(accessor, handle);
         return ApiResponse.of(SUCCESS_BLOCK_MEMBER);
     }
 
     @GetMapping("/block")
+    @MemberOnly
     public ApiResponse<BlockElements> getBlockedMembers(
+            @Auth final Accessor accessor,
             @PathVariable("handle") final String handle,
             @PageableDefault(DEFAULT_PAGING_SIZE) final Pageable pageable
     ) {
-        return ApiResponse.onSuccess(blockService.getBlockedMember(handle, pageable));
+        return ApiResponse.onSuccess(blockService.getBlockedMember(
+                accessor,
+                handle,
+                pageable
+        ));
     }
 
     @DeleteMapping("/block")
+    @MemberOnly
     public ApiResponse<Void> cancelBlock(
+            @Auth final Accessor accessor,
             @PathVariable("handle") final String handle,
             @RequestParam("blockedMemberHandle") final String blockedMemberHandle
     ) {
-        blockService.cancelBlock(handle, blockedMemberHandle);
+        blockService.cancelBlock(
+                accessor,
+                handle,
+                blockedMemberHandle
+        );
         return ApiResponse.of(SUCCESS_CANCEL_BLOCK);
     }
 }
