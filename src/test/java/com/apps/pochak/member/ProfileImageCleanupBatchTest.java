9106cfee018899c5694e3a8b393d2ec368ff416c
영서 <dudrhy12@sookmyung.ac.kr>
2024-09-02 01:11:04 +0900

    #35 배치작업 Test 추가

---
+++ src/test/java/com/apps/pochak/member/ProfileImageCleanupBatchTest.java
@@ -0,0 +1,63 @@
+package com.apps.pochak.member;
+
+import com.apps.pochak.member.fixture.MemberFixture;
+import com.apps.pochak.global.s3.S3Service;
+import com.apps.pochak.member.service.ProfileImageCleanupBatch;
+import com.apps.pochak.member.service.ProfileImageDeletionQueue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+public class ProfileImageCleanupBatchTest {
+
+    @Mock
+    private S3Service awsS3Service;
+
+    @Mock
+    private ProfileImageDeletionQueue profileImageDeletionQueue;
+
+    @InjectMocks
+    private ProfileImageCleanupBatch profileImageCleanupBatch;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    @DisplayName("이전 프로필 사진을 삭제한다")
+    void testCleanUpOldProfileImage() {
+        String oldImageUrl = MemberFixture.MEMBER1.getProfileImage();
+        when(profileImageDeletionQueue.getImagesToDelete()).thenReturn(List.of(oldImageUrl));
+
+        profileImageCleanupBatch.cleanUpOldProfileImages();
+
+        verify(awsS3Service, times(1)).deleteFileFromS3(oldImageUrl);
+        verify(profileImageDeletionQueue, times(1)).clearProcessedImages(List.of(oldImageUrl));
+    }
+
+    @Test
+    @DisplayName("여러번 바뀐 이전 프로필 사진을 삭제한다.")
+    void testCleanUpMultipleOldProfileImages() {
+        String oldImageUrl1 = "http://example.com/oldImage1.jpg";
+        String oldImageUrl2 = "http://example.com/oldImage2.jpg";
+        String oldImageUrl3 = "http://example.com/oldImage3.jpg";
+
+        when(profileImageDeletionQueue.getImagesToDelete()).thenReturn(List.of(oldImageUrl1, oldImageUrl2, oldImageUrl3));
+
+        profileImageCleanupBatch.cleanUpOldProfileImages();
+
+        verify(awsS3Service, times(1)).deleteFileFromS3(oldImageUrl1);
+        verify(awsS3Service, times(1)).deleteFileFromS3(oldImageUrl2);
+        verify(awsS3Service, times(1)).deleteFileFromS3(oldImageUrl3);
+
+        verify(profileImageDeletionQueue, times(1)).clearProcessedImages(List.of(oldImageUrl1, oldImageUrl2, oldImageUrl3));
+    }
+}
