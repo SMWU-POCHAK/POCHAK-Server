565c587a11c92007d6267fecc6d319300d79962c
yeahjinjeong <jjinnyzzang@gmail.com>
2024-09-24 15:32:37 +0900

    Update: 추억 페이지 jpa 테스트에 사용할 MemberFixture 추가

--- src/test/java/com/apps/pochak/member/fixture/MemberFixture.java
+++ src/test/java/com/apps/pochak/member/fixture/MemberFixture.java
@@ -33,8 +33,21 @@ public class MemberFixture {
             "SOCIAL_REFRESH_TOKEN"
     );
 
-    public static final Member BLOCKED_MEMBER = new Member(
+    public static final Member MEMBER3 = new Member(
             3L,
+            "member3",
+            "3번 회원의 이름",
+            "한 줄 소개",
+            "aaa@pochak.com",
+            PROFILE_IMAGE,
+            "REFRESH_TOKEN",
+            "SOCIAL_ID",
+            SocialType.APPLE,
+            "SOCIAL_REFRESH_TOKEN"
+    );
+
+    public static final Member BLOCKED_MEMBER = new Member(
+            4L,
             "blocked_member",
             "차단된 회원의 이름",
             "한 줄 소개",
