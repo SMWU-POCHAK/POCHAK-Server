51cd0dbe3ed6ab8d9c50e488e381180f26ddfddc
Lucy Oh <56earls@gmail.com>
2024-09-29 01:46:00 +0900

    Refactor: Fixture static한 것과 분리 #67

--- src/test/java/com/apps/pochak/member/fixture/MemberFixture.java
+++ src/test/java/com/apps/pochak/member/fixture/MemberFixture.java
@@ -7,9 +7,9 @@ public class MemberFixture {
 
     private static final String PROFILE_IMAGE = "https://avatars.githubusercontent.com/u/163827369?s=200&v=4";
 
-    public static final Member MEMBER1 = new Member(
+    public static final Member STATIC_MEMBER1 = new Member(
             1L,
-            "member1",
+            "static-member1",
             "1번 회원의 이름",
             "한 줄 소개",
             "aaa@pochak.com",
@@ -20,9 +20,9 @@ public class MemberFixture {
             "SOCIAL_REFRESH_TOKEN"
     );
 
-    public static final Member MEMBER2 = new Member(
+    public static final Member STATIC_MEMBER2 = new Member(
             2L,
-            "member2",
+            "static-member2",
             "2번 회원의 이름",
             "한 줄 소개",
             "aaa@pochak.com",
@@ -33,29 +33,51 @@ public class MemberFixture {
             "SOCIAL_REFRESH_TOKEN"
     );
 
-    public static final Member MEMBER3 = new Member(
-            3L,
-            "member3",
-            "3번 회원의 이름",
-            "한 줄 소개",
-            "aaa@pochak.com",
-            PROFILE_IMAGE,
-            "REFRESH_TOKEN",
-            "SOCIAL_ID",
-            SocialType.APPLE,
-            "SOCIAL_REFRESH_TOKEN"
-    );
+    public static final Member OWNER = Member.signupMember()
+            .name("게시물 업로드한 사람")
+            .email("aaa@pochak.com")
+            .handle("owner")
+            .message("한 줄 소개")
+            .socialId("SOCIAL_ID")
+            .profileImage(PROFILE_IMAGE)
+            .refreshToken("REFRESH_TOKEN")
+            .socialType(SocialType.GOOGLE)
+            .socialRefreshToken("SOCIAL_REFRESH_TOKEN")
+            .build();
 
-    public static final Member BLOCKED_MEMBER = new Member(
-            4L,
-            "blocked_member",
-            "차단된 회원의 이름",
-            "한 줄 소개",
-            "aaa@pochak.com",
-            PROFILE_IMAGE,
-            "REFRESH_TOKEN",
-            "SOCIAL_ID",
-            SocialType.APPLE,
-            "SOCIAL_REFRESH_TOKEN"
-    );
+    public static final Member TAGGED_MEMBER1 = Member.signupMember()
+            .name("태그된 사람 1번")
+            .email("aaa@pochak.com")
+            .handle("tagged_member1")
+            .message("한 줄 소개")
+            .socialId("SOCIAL_ID")
+            .profileImage(PROFILE_IMAGE)
+            .refreshToken("REFRESH_TOKEN")
+            .socialType(SocialType.GOOGLE)
+            .socialRefreshToken("SOCIAL_REFRESH_TOKEN")
+            .build();
+
+    public static final Member TAGGED_MEMBER2 = Member.signupMember()
+            .name("태그된 사람 2번")
+            .email("aaa@pochak.com")
+            .handle("tagged_member1")
+            .message("한 줄 소개")
+            .socialId("SOCIAL_ID")
+            .profileImage(PROFILE_IMAGE)
+            .refreshToken("REFRESH_TOKEN")
+            .socialType(SocialType.GOOGLE)
+            .socialRefreshToken("SOCIAL_REFRESH_TOKEN")
+            .build();
+
+    public static final Member LOGIN_MEMBER = Member.signupMember()
+            .name("로그인한 사람")
+            .email("aaa@pochak.com")
+            .handle("login_member")
+            .message("한 줄 소개")
+            .socialId("SOCIAL_ID")
+            .profileImage(PROFILE_IMAGE)
+            .refreshToken("REFRESH_TOKEN")
+            .socialType(SocialType.GOOGLE)
+            .socialRefreshToken("SOCIAL_REFRESH_TOKEN")
+            .build();
 }
