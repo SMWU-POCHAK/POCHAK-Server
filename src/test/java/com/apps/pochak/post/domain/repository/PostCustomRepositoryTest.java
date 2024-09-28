308c90ae51e238512c562308bf4e25fa13f1ea6f
Lucy Oh <56earls@gmail.com>
2024-09-28 14:04:50 +0900

    Rename: Querydsl 이름 변경 #67

--- src/test/java/com/apps/pochak/post/domain/repository/PostCustomRepositoryTest.java
+++ src/test/java/com/apps/pochak/post/domain/repository/PostCustomRepositoryTest.java
@@ -1,17 +1,19 @@
 package com.apps.pochak.post.domain.repository;
 
-import com.apps.pochak.global.TestQueryDSLConfig;
+import com.apps.pochak.global.TestQuerydslConfig;
 import com.apps.pochak.member.domain.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
 @DataJpaTest
-@Import(TestQueryDSLConfig.class)
+@Import(TestQuerydslConfig.class)
 class PostCustomRepositoryTest {
     @Autowired
     PostCustomRepository postCustomRepository;
 
     @Autowired
     MemberRepository memberRepository;
+
+    
 }
