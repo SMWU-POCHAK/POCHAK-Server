bbccfb391c0867cd2cb00daad7a62eacd9711b19
Lucy Oh <56earls@gmail.com>
2024-10-03 02:36:38 +0900

    Refactor: `getHomeTab` querydsl로 리팩토링

--- src/main/java/com/apps/pochak/post/domain/repository/PostCustomRepository.java
+++ src/main/java/com/apps/pochak/post/domain/repository/PostCustomRepository.java
@@ -1,16 +1,22 @@
 package com.apps.pochak.post.domain.repository;
 
-import com.apps.pochak.global.BaseEntityStatus;
 import com.apps.pochak.global.api_payload.exception.GeneralException;
 import com.apps.pochak.post.domain.Post;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import static com.apps.pochak.block.domain.QBlock.block;
+import static com.apps.pochak.follow.domain.QFollow.follow;
+import static com.apps.pochak.global.BaseEntityStatus.ACTIVE;
 import static com.apps.pochak.global.api_payload.code.status.ErrorStatus.BLOCKED_POST;
 import static com.apps.pochak.post.domain.QPost.post;
 import static com.apps.pochak.tag.domain.QTag.tag;
@@ -35,18 +41,71 @@ public class PostCustomRepository {
                 query.selectFrom(post)
                         .join(post.owner).fetchJoin()
                         .join(tag).on(tag.post.eq(post))
-                        .leftJoin(block).on(
-                                checkOwnerOrTaggedMemberBlockLoginMember(loginMemberId)
-                                        .or(checkLoginMemberBlockOwnerOrTaggedMember(loginMemberId))
-                        )
+                        .leftJoin(block).on(checkBlockStatus(loginMemberId))
                         .groupBy(post)
                         .having(block.id.count().eq(0L))
                         .where(
                                 post.id.eq(postId),
-                                post.status.eq(BaseEntityStatus.ACTIVE)
+                                post.status.eq(ACTIVE)
                         )
                         .fetchOne()
         );
+    }
+
+    public Page<Post> findPostOfFollowing(
+            final Long memberId,
+            final Pageable pageable
+    ) {
+        List<Post> postList = findPostOfFollowing(memberId)
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        // TODO: 이게 최선인지.. 확인하기
+        Long count = query
+                .select(post.count())
+                .from(post)
+                .where(post.in(findPostOfFollowing(memberId)))
+                .fetchOne();
+
+        return new PageImpl<>(postList, pageable, count);
+    }
+
+    private JPQLQuery<Post> findPostOfFollowing(final Long memberId) {
+        return query
+                .selectFrom(post)
+                .join(tag).on(
+                        tag.post.eq(post)
+                                .and(tag.status.eq(ACTIVE))
+                                .and(post.status.eq(ACTIVE))
+                )
+                .leftJoin(follow).on(checkFollowOwnerOrTaggedMember(memberId))
+                .leftJoin(block).on(checkBlockStatus(memberId))
+                .groupBy(post)
+                .having((follow.id.count().gt(0L).or(post.owner.id.eq(memberId).or(tag.member.id.eq(memberId))))
+                        .and(block.id.count().eq(0L)))
+                .orderBy(post.allowedDate.desc());
+    }
+
+    private BooleanExpression checkFollowOwnerOrTaggedMember(final Long memberId) {
+        return checkFollowOwner(memberId).or(checkFollowTaggedMember(memberId));
+    }
+
+    private BooleanExpression checkFollowOwner(final Long loginMemberId) {
+        return follow.receiver.eq(post.owner)
+                .and(follow.sender.id.eq(loginMemberId))
+                .and(follow.status.eq(ACTIVE));
+    }
+
+    private BooleanExpression checkFollowTaggedMember(final Long loginMemberId) {
+        return follow.receiver.eq(tag.member)
+                .and(follow.sender.id.eq(loginMemberId))
+                .and(follow.status.eq(ACTIVE));
+    }
+
+    private BooleanExpression checkBlockStatus(final Long loginMemberId) {
+        return checkOwnerOrTaggedMemberBlockLoginMember(loginMemberId)
+                .or(checkLoginMemberBlockOwnerOrTaggedMember(loginMemberId));
     }
 
     private BooleanExpression checkOwnerOrTaggedMemberBlockLoginMember(final Long loginMemberId) {
