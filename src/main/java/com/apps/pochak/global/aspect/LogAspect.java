f8deb5b9ab9722a5ac1b2e907a54c832dcec6208
yeahjinjeong <jjinnyzzang@gmail.com>
2024-11-19 16:22:25 +0900

    Update: 응답 상태 로그남기기 #6

--- src/main/java/com/apps/pochak/global/aspect/LogAspect.java
+++ src/main/java/com/apps/pochak/global/aspect/LogAspect.java
@@ -1,24 +1,18 @@
 package com.apps.pochak.global.aspect;
 
+import com.apps.pochak.global.api_payload.ApiResponse;
 import com.apps.pochak.global.api_payload.exception.GeneralException;
-import com.apps.pochak.global.util.RequestInfo;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
-import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-import org.springframework.web.context.request.WebRequest;
-
-import java.util.Objects;
 
 import static com.apps.pochak.global.util.RequestInfo.createRequestFullPath;
 
@@ -58,16 +52,16 @@ public class LogAspect {
         Object response;
         try {
             response = joinPoint.proceed();
+            ApiResponse<?> entity = (ApiResponse<?>) response;
+            log.info("Response status code = {}", entity.getCode());
         } catch (GeneralException e) {
             log.error("Exception occurred in method = {}, Error message = {}",
                     joinPoint.getSignature().getName(), e.getMessage());
             throw e;
         } finally {
             long executionTime = System.currentTimeMillis() - startAt;
-            log.info("Execution time : {} ms", executionTime);
+            log.info("Execution time = {} ms", executionTime);
         }
-        log.info("Response Return type = {}, Return value = {}", response.getClass().getSimpleName(), response);
-
         return response;
     }
 
