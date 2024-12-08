38cf0a236670de700561e19a58bff176dafc3308
Lucy Oh <56earls@gmail.com>
2024-12-08 21:38:53 +0900

    Test: 알람 조회 테스트 #142

--- src/main/java/com/apps/pochak/alarm/dto/response/AlarmElement.java
+++ src/main/java/com/apps/pochak/alarm/dto/response/AlarmElement.java
@@ -2,20 +2,18 @@ package com.apps.pochak.alarm.dto.response;
 
 import com.apps.pochak.alarm.domain.Alarm;
 import com.apps.pochak.alarm.domain.AlarmType;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
+@Data
 public abstract class AlarmElement {
     private Long alarmId;
     private AlarmType alarmType;
     private Boolean isChecked;
     private LocalDateTime createdDate;
 
-    protected AlarmElement (Alarm alarm) {
+    protected AlarmElement(Alarm alarm) {
         this.alarmId = alarm.getId();
         this.alarmType = alarm.getAlarmType();
         this.isChecked = alarm.getIsChecked();
---
+++ src/test/java/com/apps/pochak/alarm/service/AlarmServiceTest.java
@@ -0,0 +1,109 @@
+package com.apps.pochak.alarm.service;
+
+import com.apps.pochak.alarm.domain.repository.AlarmRepository;
+import com.apps.pochak.alarm.dto.response.AlarmElements;
+import com.apps.pochak.auth.domain.Accessor;
+import com.apps.pochak.global.image.CloudStorageService;
+import com.apps.pochak.member.domain.Member;
+import com.apps.pochak.member.domain.repository.MemberRepository;
+import com.apps.pochak.post.domain.Post;
+import com.apps.pochak.post.domain.repository.PostRepository;
+import com.apps.pochak.post.dto.request.PostUploadRequest;
+import com.apps.pochak.post.service.PostService;
+import com.apps.pochak.tag.domain.repository.TagRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static com.apps.pochak.global.Constant.DEFAULT_PAGING_SIZE;
+import static com.apps.pochak.global.MockMultipartFileConverter.getMockMultipartFileOfPost;
+import static com.apps.pochak.member.fixture.MemberFixture.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@Transactional
+class AlarmServiceTest {
+
+    @Autowired
+    AlarmService alarmService;
+
+    @Autowired
+    PostService postService;
+
+    @Autowired
+    AlarmRepository alarmRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    TagRepository tagRepository;
+
+    @Autowired
+    PostRepository postRepository;
+
+    @MockBean
+    CloudStorageService cloudStorageService;
+
+    private Member owner;
+    private Member taggedMember1;
+    private Member taggedMember2;
+    private Member loginMember;
+
+    @BeforeEach
+    void setUp() {
+        owner = memberRepository.save(OWNER);
+        taggedMember1 = memberRepository.save(TAGGED_MEMBER1);
+        taggedMember2 = memberRepository.save(TAGGED_MEMBER2);
+        loginMember = memberRepository.save(LOGIN_MEMBER);
+    }
+
+    @DisplayName("알람이 정상적으로 조회된다.")
+    @Test
+    void getAllAlarms() throws Exception {
+        // given
+        when(cloudStorageService.upload(any(), any()))
+                .thenReturn("");
+
+        savePost();
+
+        AlarmElements expected = new AlarmElements(
+                alarmRepository.getAllAlarm(
+                        taggedMember1.getId(),
+                        PageRequest.of(0, DEFAULT_PAGING_SIZE)
+                ));
+
+        // when
+        AlarmElements actual = alarmService.getAllAlarms(
+                Accessor.member(taggedMember1.getId()),
+                PageRequest.of(0, DEFAULT_PAGING_SIZE)
+        );
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    private Post savePost() throws Exception {
+        PostUploadRequest request = new PostUploadRequest(
+                getMockMultipartFileOfPost(),
+                "test caption",
+                List.of(taggedMember1.getHandle(), taggedMember2.getHandle())
+        );
+
+        postService.savePost(
+                Accessor.member(owner.getId()),
+                request
+        );
+
+        return postRepository.findAll().get(0);
+    }
+}
