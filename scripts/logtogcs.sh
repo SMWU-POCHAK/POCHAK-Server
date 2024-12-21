437a779e0014668ed314d1316de7c23aa12ccd08
yeahjinjeong <yeahjin311@sookmyung.ac.kr>
2024-12-21 13:28:21 +0900

    [Fix/#6-logging] 로그 버킷 업로드 쉘 스크립트 수정

--- scripts/logtogcs.sh
+++ scripts/logtogcs.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-gsutil -m  cp /var/log/pochak-prod/application.*.log gs://pochak-log-bucket/application/
+snap/bin/gsutil -m  cp /var/log/pochak-prod/application.*.log gs://pochak-log-bucket/application/
 # application.*.log 버킷 업로드
-gsutil -m cp /var/log/pochak-prod/error.*.log gs://pochak-log-bucket/error/
+snap/bin/gsutil -m cp /var/log/pochak-prod/error.*.log gs://pochak-log-bucket/error/
 # error.*.log 버킷 업로드
 
 echo "GCS 업로드 완료"
 
-rm -f /var/log/pochak-prod/application*.log
-rm -f /var/log/pochak-prod/error*.log
-
-echo "로컬 파일 삭제 완료"
+# crontab -l 로 0 0 * * * /home/ubuntu/logtogcs.sh 자정 실행 command 확인 가능
+# /var/log/cron.log 에서 크론탭 실행 로그 확인 가능
