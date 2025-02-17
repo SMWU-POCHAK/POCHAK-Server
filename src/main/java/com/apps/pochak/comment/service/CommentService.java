6a9494d5233317c9b8f58f542d12e08aa22b5b74
yeahjinjeong <jjinnyzzang@gmail.com>
2025-02-17 23:34:01 +0900

    Fix: toPage 페이징사이즈 변경 #189

--- src/main/java/com/apps/pochak/comment/service/CommentService.java
+++ src/main/java/com/apps/pochak/comment/service/CommentService.java
@@ -15,10 +15,8 @@ import com.apps.pochak.post.domain.repository.PostRepository;
 import com.apps.pochak.tag.domain.Tag;
 import com.apps.pochak.tag.domain.repository.TagRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
